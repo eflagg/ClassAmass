2a8918a7e03ffe09655c9526ec2b83965ffafcce
Emily Flagg <eflagg5@gmail.com>
2016-09-16 23:43:07 +0000

    changed host and port

--- server.py
+++ server.py
@@ -417,5 +417,6 @@ if __name__ == "__main__":
     connect_to_db(app)
 
     # DebugToolbarExtension(app)
+    PORT = int(os.environ.get("PORT", 5000))
 
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=PORT)
